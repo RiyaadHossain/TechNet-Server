cf6086b469d26f98e28038b615f64769540a9dbe
RiyaadHossain <riyadhossain.dev@gmail.com>
2023-07-07 11:00:37 +0600

    mongodb uri string modified

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.euxm4cs.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
+const client = new MongoClient(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
@@ -18,8 +17,8 @@ const client = new MongoClient(uri, {
 
 const run = async () => {
   try {
-    const db = client.db('tech-net');
-    const productCollection = db.collection('product');
+    const db = client.db('Tech-Net');
+    const productCollection = db.collection('products');
 
     app.get('/products', async (req, res) => {
       const cursor = productCollection.find({});
@@ -38,9 +37,7 @@ const run = async () => {
 
     app.get('/product/:id', async (req, res) => {
       const id = req.params.id;
-
       const result = await productCollection.findOne({ _id: ObjectId(id) });
-      console.log(result);
       res.send(result);
     });
 
@@ -48,7 +45,6 @@ const run = async () => {
       const id = req.params.id;
 
       const result = await productCollection.deleteOne({ _id: ObjectId(id) });
-      console.log(result);
       res.send(result);
     });
 
@@ -56,23 +52,17 @@ const run = async () => {
       const productId = req.params.id;
       const comment = req.body.comment;
 
-      console.log(productId);
-      console.log(comment);
-
       const result = await productCollection.updateOne(
         { _id: ObjectId(productId) },
         { $push: { comments: comment } }
       );
 
-      console.log(result);
 
       if (result.modifiedCount !== 1) {
-        console.error('Product not found or comment not added');
         res.json({ error: 'Product not found or comment not added' });
         return;
       }
 
-      console.log('Comment added successfully');
       res.json({ message: 'Comment added successfully' });
     });
   } finally {
